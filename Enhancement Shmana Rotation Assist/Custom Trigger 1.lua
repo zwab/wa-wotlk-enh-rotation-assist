45f8c90f94fe9092ea47608721fc70d1465ec5c9
zwab <51080317+zwab@users.noreply.github.com>
2022-10-27 11:46:30 +0100

    Refactor
     - Bumped fire nova duration to 3 to account for Glyph
     - Updated fire totem logic to suggest fire totem as next GCD when it has 1 second remaining
     - Refactored logic determining next GCD

--- Enhancement Shmana Rotation Assist/Custom Trigger 1.lua
+++ Enhancement Shmana Rotation Assist/Custom Trigger 1.lua
@@ -5,9 +5,10 @@ function (event, unit, subevent, spellid)
     
     local function setsuggestionandicon(spell_name)
         aura_env.suggestion = spell_name
-        aura_env.icon = select(3, GetSpellInfo(aura_env.suggestion))
-        if aura_env.suggest == "Free GCD LOL" then
+        if aura_env.suggestion == "Free GCD LOL" then
             aura_env.icon = 136221
+        else
+            aura_env.icon = select(3, GetSpellInfo(aura_env.suggestion))
         end
         return true
     end
@@ -84,95 +85,89 @@ function (event, unit, subevent, spellid)
         shockstart, shockduration = GetSpellCooldown("Flame Shock")
         if (shockduration == 6) then
             shockcd = shockstart + shockduration
-        else
-            shockcd = shockstart + gcd
+        else if aura_env.shocks[aura_env.spell] then
+                shockcd = time + 6
+            else
+                shockcd = shockstart + gcd
+            end
         end
-        
+            
         -- Calculate CD info of Stormstrike
         local stormcd, stormstart, stormduration
         stormstart, stormduration = GetSpellCooldown("Stormstrike")
         if (stormduration == 8) then
             stormcd = stormstart + stormduration
+        elseif aura_env.spell == "Stormstrike" then
+            stormcd = time + 8
         else
             stormcd = stormstart + gcd
         end
-        
+            
+        -- Calculate if Fire Totem expiration is less than a gcd
+        local firetotemcd = GetTotemTimeLeft(1)
+
         -- Calculate CD info of Fire Nova
         local novacd, novastart, novaduration
         novastart, novaduration = GetSpellCooldown("Fire Nova")
-        if (novaduration == 6) then
+        if (novaduration == 3) then
             novacd = novastart + novaduration
+        elseif aura_env.spell == "Fire Nova" then
+            novacd = time + 3
         else
             novacd = novastart + gcd
         end
-        
+            
         -- Calculate CD info of Lava Lash
         local lashcd, lashstart, lashduration
         lashstart, lashduration = GetSpellCooldown("Lava Lash")
         if (lashduration == 6) then
             lashcd = lashstart + lashduration
+        elseif aura_env.spell == "Lava Lash" then
+            lashcd = time + 6
         else
             lashcd = lashstart + gcd
         end
+
         
-        
-        -- Decide if we should shock and which one to use
-        if not aura_env.shocks[aura_env.spell] then
-            if checkcd(shockcd) then
-                if not checkdot() then
-                    if setsuggestionandicon("Flame Shock") then
-                        return true
-                    end 
-                else
-                    if setsuggestionandicon("Earth Shock") then
-                        return true
-                    end
+        -- Determine what is coming up within the next GCD
+        local shockgcd = checkcd(shockcd)
+        local stormgcd = checkcd(stormcd)
+        local firetotemgcd = firetotemcd <= 1
+        local novagcd = checkcd(novacd)
+        local lashgcd = checkcd(lashcd)
+
+        -- Suggest Action for next GCD
+        if shockgcd then
+            if not checkdot() then
+                if setsuggestionandicon("Flame Shock") then
+                    return true
                 end
-            end
-        end
-        
-        -- Decide if to use Stormstrike
-        if aura_env.spell ~= "Stormstrike" then
-            if checkcd(stormcd) then
-                if setsuggestionandicon("Stormstrike") then
+            else
+                if setsuggestionandicon("Earth Shock") then
                     return true
                 end
             end
-        end
-        
-        -- Place a Fire Totem
-        if not aura_env.firetotems[aura_env.spell] then
-            if GetTotemTimeLeft(1) == 0 then
-                if setsuggestionandicon("Magma Totem") then
-                    return true
-                end
+        elseif stormgcd then
+            if setsuggestionandicon("Stormstrike") then
+                return true
             end
-        end
-        
-        -- Decide if we should Fire Nova
-        if GetTotemTimeLeft(1) ~= 0 then
-            if aura_env.spell ~= "Fire Nova" then
-                if checkcd(novacd) then
-                    if setsuggestionandicon("Fire Nova") then
-                        return true
-                    end
-                end
+        elseif firetotemgcd then
+            if setsuggestionandicon("Magma Totem") then
+                return true
             end
-        end
-        
-        -- Lava Lash LOL
-        if aura_env.spell ~= "Lava Lash" then
-            if checkcd(lashcd) then
-                if setsuggestionandicon("Lava Lash") then
-                    return true
-                end
+        elseif novagcd then
+            if setsuggestionandicon("Fire Nova") then
+                return true
             end
-        end
-        
-        if setsuggestionandicon("Free GCD LOL") then
-            return true
-        end
-        
+        elseif lashgcd then
+            if setsuggestionandicon("Lava Lash") then
+                return true
+            end
+        else
+            if setsuggestionandicon("Free GCD LOL") then
+                return true
+            end
+        end    
     end
-    
 end
+
